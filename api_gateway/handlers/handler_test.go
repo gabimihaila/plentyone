7ab96ef0b9bae021be6203e7f9f9beb388661df5
Gabriela Mihăilă <gabrielamihaila@MacBook-Pro.local>
2025-03-24 10:53:02 +0200

    started handler unit test

---
+++ api_gateway/handlers/handler_test.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"api_gateway/config"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var expectedConfigFile = "../config-test.json"
+
+func TestGetTargetUrl(t *testing.T) {
+	expectedDestinations := []config.Destination{
+		{
+			URL:        "http://192.168.1.999:8086",
+			PathPrefix: "serv25",
+		},
+	}
+
+	expectedPath := "serv25"
+
+	expectedUrl := "http://192.168.1.999:8086"
+
+	url := getTargetUrl(expectedDestinations, expectedPath)
+
+	if url != expectedUrl {
+		t.Errorf("Expected url %s, got %s", expectedUrl, url)
+	}
+}
+
+var ExpectedConfig = config.LoadConfig(expectedConfigFile)
